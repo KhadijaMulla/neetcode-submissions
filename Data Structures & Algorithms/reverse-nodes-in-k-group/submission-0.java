c99a387957bd6acc4d34bd8f5687c00889660b52
KhadijaMulla <khadijamulla93493@gmail.com>
2026-08-15 17:16:09 +0530

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        ListNode temp = head;
+
+        // Check if there are k nodes
+        for (int i = 0; i < k; i++) {
+            if (temp == null) {
+                return head;
+            }
+            temp = temp.next;
+        }
+
+        // Reverse k nodes
+        ListNode prev = null;
+        ListNode curr = head;
+
+        for (int i = 0; i < k; i++) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // head is now the last node of reversed group
+        head.next = reverseKGroup(curr, k);
+
+        return prev;
+    }
+}
